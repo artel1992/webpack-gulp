2c9537c5242649217832a6d33f7e05f551ba5aef
Артем <artela1992@gmail.com>
2022-06-29 12:35:00 +0300

    настроил watching

--- gulpfile.js
+++ gulpfile.js
@@ -8,17 +8,30 @@ const webpack = require('webpack')
 const webpackConfig = require('./webpack.config.js')
 const sass = require('gulp-sass')(require('sass'))
 const autoprefixer = require("gulp-autoprefixer")
+const browserSync = require("browser-sync").create()
 
-const assetsTask = () => gulp.src(['./public/{img,fonts}/*.*']).pipe(gulp.dest('dist-gulp'))
+const paths = {
+    buildDirectory: './dist',
+    assets: './public/{img,fonts}/*.*',
+    styles: './src/styles/**/*.{sass,scss}',
+    webpack: ['./src/**/*.*', './public/index.html'],
+}
+
+const assetsTask = () =>
+    gulp.src([paths.assets])
+        .pipe(gulp.dest(paths.buildDirectory))
+        .pipe(browserSync.stream())
+
 const sassTask = () =>
-    gulp.src('./src/styles/**/*.{sass,scss}')
+    gulp.src(paths.styles)
         .pipe(sass({outputStyle: "compressed"}))
         .pipe(autoprefixer())
         .pipe(concat('styles.css'))
-        .pipe(gulp.dest('dist-gulp/css'))
+        .pipe(gulp.dest(`${paths.buildDirectory}/css`))
+        .pipe(browserSync.stream())
 
 const webpackTask = () =>
-    gulp.src(['./src/**/index.{js,ts}'])
+    gulp.src(paths.webpack)
         .pipe(plumber({
             errorHandler: notify.onError(err => ({
                 title: 'Webpack',
@@ -27,11 +40,24 @@ const webpackTask = () =>
         }))
         .pipe(named())
         .pipe(webpackStream(webpackConfig(), webpack))
-        .pipe(gulp.dest('./dist-gulp'))
+        .pipe(gulp.dest(paths.buildDirectory))
+        .pipe(browserSync.stream())
 
 gulp.task('sass', sassTask)
-gulp.task('assets')
+gulp.task('assets', assetsTask)
 gulp.task('webpack', webpackTask)
 
 gulp.task('default', gulp.parallel(webpackTask, assetsTask))
+
+gulp.task('watch', (finish) => {
+    browserSync.init({
+        server: paths.buildDirectory,
+    })
+    gulp.watch(paths.webpack, webpackTask).on("change", (paths, stats) => {
+        console.log(`File ${paths} changed`)
+        // browserSync.reload()
+
+    })
+    gulp.watch([paths.assets], gulp.series(assetsTask)).on("change", browserSync.reload)
+})
 
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import {showDropMenu} from "./components/header"
 
-
+console.log()
 let headerAvatar = document.getElementById('header-avatar')
 headerAvatar.addEventListener('click', (event) => showDropMenu(event))
